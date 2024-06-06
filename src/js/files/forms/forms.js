78f81b53b2641cbc346d91bca2baa78dcdb99c34
DenisGrishin <666trall666@gmail.com>
2024-06-06 09:40:51 +0300

    комит пред парвкой косяк

---
+++ src/js/files/forms/forms.js
@@ -0,0 +1,12 @@
+// // Подключение функционала "Чертогов Фрилансера"
+// // Вспомогательные функции
+// import { isMobile, _slideUp, _slideDown, _slideToggle } from '../functions.js';
+
+// // Класс select
+// import { SelectConstructor } from '../../libs/select.js';
+
+// /* Модуль работы с select */
+// export function formSelect() {
+//   debugger;
+//   formsModules.selectModule = new SelectConstructor({});
+// }
